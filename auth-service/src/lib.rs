8a39252f9111e9e2c17153ed0927367972e0a2cc
vuongDang <vuongdang9@gmail.com>
2025-05-02 08:32:31 +0200

    [Sprint 1][Task 5] Add basic testing

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,54 @@
+use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
+use std::error::Error;
+use tower_http::services::ServeDir;
+
+// This struct encapsulates our application-related logic.
+pub struct Application {
+    server: Serve<Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let router = Router::new()
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/verify-token", post(verify_token));
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        // Create a new Application instance and return it
+        Ok(Application { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
